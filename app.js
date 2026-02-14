c625cebb9a9067ade45aa5f3e3237c2bc4b7c032
MehdiAliQoli <2022n03925@gmail.com>
2026-02-14 23:44:43 +0500

    ..

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const box = document.getElementById('board');
-
+const image = document.createElement('img');
+ image.src = "/Pieces/b_bishop_png_128px.png"
+ image.style.height = "50px";
 for(let i=0; i<8; i++){
     for(let j=0; j<8; j++){
+
         const cell = document.createElement('div')
         cell.classList.add('cell');
-        const image = document.createElement('img');
-        image.src = "/Pieces/b_bishop_png_128px.png"
-        image.style.height = "50px";
+        box.appendChild(cell);
         cell.appendChild(image)
         cell.addEventListener('click', () => {
-            cell.style.backgroundColor = "green";
+            cell.classList.toggle('active');
         });
-        box.appendChild(cell);
         
         
         
